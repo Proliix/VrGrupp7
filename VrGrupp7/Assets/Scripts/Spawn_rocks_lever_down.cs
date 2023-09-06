462f314bdb544666d0e61d89ea20bcb9a86d7708
Julia Isaksson <112895681+Juliastarstrike@users.noreply.github.com>
2023-09-06 16:05:49 +0200

    Added a limit of how many rocks the rock machine can spawn

--- VrGrupp7/Assets/Scripts/Spawn_rocks_lever_down.cs
+++ VrGrupp7/Assets/Scripts/Spawn_rocks_lever_down.cs
@@ -8,11 +8,14 @@ public class Spawn_rocks_lever_down : MonoBehaviour
     //there will spawn an Rock on position of the gamobject called Spawn Rocks.
     public GameObject prefab_rock;
     public GameObject gameobject_spawn_rocks;
+    int max_rock_in_scene = 30;
+    int current_rocks_in_scene = 0;
     
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Collision_spawn_rocks")
+        if (other.tag == "Collision_spawn_rocks" && max_rock_in_scene > current_rocks_in_scene)
         {
+        current_rocks_in_scene += 1;
         Instantiate(prefab_rock, gameobject_spawn_rocks.transform.position, Quaternion.identity);
         }
     }
