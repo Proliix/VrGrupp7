837be54e4af34ab2ea79531d8cb0acaf2928b274
Proliix <lucas.andreasson@hotmail.com>
2023-09-06 15:22:52 +0200

    Added the ability to pour liquids and mate it possible to catch the liquids

--- VrGrupp7/Assets/Scripts/LiquidContainer.cs
+++ VrGrupp7/Assets/Scripts/LiquidContainer.cs
@@ -6,18 +6,28 @@ public class LiquidContainer : MonoBehaviour
 {
     [SerializeField] GameObject liquidObject;
     [SerializeField] ParticleSystem particles;
-    [SerializeField] float filledTiltRange, minTiltRange;
-    [SerializeField] float maxLiquid, minLiquid;
+    [SerializeField] AnimationCurve flowRate;
+
+    [Header("Tuneing")]
+    [SerializeField] float minimumFillAmount = 0;
+    [SerializeField] float emptySpeed = 0.1f;
+    [SerializeField] float bigPourMultiplier = 0.75f;
 
     bool hasParticles;
     float angle;
     float fillAmount;
+    Vector3 wobblePos;
     Material mat;
+
+
+    float forceEmptyAmount = -10;
 
     // Start is called before the first frame update
     void Start()
     {
+        ParticleSystem.MainModule mainModule = particles.main;
         mat = liquidObject.GetComponent<MeshRenderer>().material;
+        mainModule.startColor = mat.GetColor("_SideColor");
     }
 
     void StartParticles()
@@ -32,23 +42,31 @@ public class LiquidContainer : MonoBehaviour
         particles.Stop();
     }
 
+    void ForceEmpty()
+    {
+        if (mat.GetFloat("_Fill") > 0)
+            mat.SetFloat("_Fill", fillAmount - (emptySpeed * 3) * Time.deltaTime);
+        else
+            mat.SetFloat("_Fill", forceEmptyAmount);
+    }
 
     // Update is called once per frame
     void Update()
     {
         fillAmount = mat.GetFloat("_Fill");
 
-        if (fillAmount > 0)
+        if (fillAmount >= minimumFillAmount)
         {
+            //finds wooble pos
+            wobblePos = new Vector3(mat.GetFloat("_WobbleX"), 0, mat.GetFloat("_WobbleZ"));
+
             //find angle for it to start to remove liquid
-            angle = Mathf.Lerp(minTiltRange, filledTiltRange, fillAmount);
+            angle = flowRate.Evaluate(fillAmount);
 
-            //Debug.Log("Angle to spill: " + angle + " | current angle: " + Vector3.Angle(transform.up, Vector3.up));
-
-            //check if it is tilted enough for it to spill then start to remove liquid
-            if (Vector3.Angle(transform.up, Vector3.up) >= angle)
+            //check if it is tilted enough for it to spill then start to remove liquid and check if the wobble would make it spill
+            if (Vector3.Angle(transform.up + wobblePos, Vector3.up) >= angle)
             {
-                mat.SetFloat("_Fill", fillAmount - (0.1f * (angle / Vector3.Angle(transform.up, Vector3.up))) * Time.deltaTime);
+                mat.SetFloat("_Fill", fillAmount - (emptySpeed * ((Vector3.Angle(transform.up, Vector3.up) / angle) * bigPourMultiplier)) * Time.deltaTime);
                 if (!hasParticles)
                 {
                     StartParticles();
@@ -59,9 +77,23 @@ public class LiquidContainer : MonoBehaviour
                 StopParticles();
             }
         }
-        else if (hasParticles)
+        else if (fillAmount > forceEmptyAmount)
         {
-            StopParticles();
+            if (hasParticles)
+                StopParticles();
+
+            ForceEmpty();
         }
     }
+
+    public Color GetSideColor()
+    {
+        return mat.GetColor("_SideColor");
+    }
+
+    public Color GetTopColor()
+    {
+        return mat.GetColor("_TopColor");
+    }
+
 }
