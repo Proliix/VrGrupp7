082cc2e1fc7656ad2cd8c07b7b5392d90bddad91
Julia Isaksson <112895681+Juliastarstrike@users.noreply.github.com>
2023-09-06 15:46:46 +0200

    Added comments and removed unnecessary lines in some scripts

--- VrGrupp7/Assets/Scripts/Spawn_rocks_lever_down.cs
+++ VrGrupp7/Assets/Scripts/Spawn_rocks_lever_down.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class Spawn_rocks_lever_down : MonoBehaviour
 {
+    //Script description: When player pulls down the lever and touch invisible trigger box
+    //there will spawn an Rock on position of the gamobject called Spawn Rocks.
     public GameObject prefab_rock;
-    public GameObject gameobject_spawn_pos;
-    public Vector3 spawn_pos;
-
+    public GameObject gameobject_spawn_rocks;
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Collision_spawn_rocks")
         {
-        spawn_pos = gameobject_spawn_pos.transform.position;
-        Instantiate(prefab_rock, spawn_pos, Quaternion.identity);
+        Instantiate(prefab_rock, gameobject_spawn_rocks.transform.position, Quaternion.identity);
         }
     }
 }
