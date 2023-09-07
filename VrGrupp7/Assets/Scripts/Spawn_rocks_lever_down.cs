3271b84947395cf97df276641fc2516b03699c9e
Julia Isaksson <112895681+Juliastarstrike@users.noreply.github.com>
2023-09-07 12:05:04 +0200

    Added sound when the rickmachine spawns rocks

--- VrGrupp7/Assets/Scripts/Spawn_rocks_lever_down.cs
+++ VrGrupp7/Assets/Scripts/Spawn_rocks_lever_down.cs
@@ -8,17 +8,20 @@ public class Spawn_rocks_lever_down : MonoBehaviour
     //there will spawn an Rock on position of the gamobject called Spawn Rocks.
     public GameObject prefab_rock;
     public GameObject gameobject_spawn_rocks;
-    public AudioSource audioSource;
+    public AudioSource Lever_audioSource;
     public AudioClip lever_sound;
+    public AudioSource rock_machin_audioSource;
+    public AudioClip rock_machin_sound;
     int max_rock_in_scene = 30;
     int current_rocks_in_scene = 0;
-    float timer_takes_to_spawn_rock = 4.0f;
+    float timer_takes_to_spawn_rock = 4.5f;
     
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Collision_spawn_rocks" && max_rock_in_scene > current_rocks_in_scene)
         {
-            audioSource.PlayOneShot(lever_sound);
+            Lever_audioSource.PlayOneShot(lever_sound);
+            rock_machin_audioSource.PlayOneShot(rock_machin_sound);
             Invoke("Instantiate_Rock", timer_takes_to_spawn_rock);
         }
     }
