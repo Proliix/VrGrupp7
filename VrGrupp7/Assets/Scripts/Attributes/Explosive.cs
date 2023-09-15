8a3fc43addc848ad162ea478600284c31bb838a3
Thomas Enghult <thomas.enghult@gmail.com>
2023-09-15 13:33:18 +0200

    implemented Interface Attribute to explosive

--- VrGrupp7/Assets/Scripts/Attributes/Explosive.cs
+++ VrGrupp7/Assets/Scripts/Attributes/Explosive.cs
@@ -3,21 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Collider))]
-public class Explosive : MonoBehaviour, IScannable
+public class Explosive : MonoBehaviour, IScannable, IAttribute
 {
 
     Rigidbody m_rb;
-    [Range(0f, 20f)] public float forceRequiredToExplode = 5f;
+    [Range(0f, 20f)] public float maxForceRequiredToExplode = 20f;
+
+    [Range(0f, 1f)] private float potency = 0;
+
     [SerializeField] private GameObject explosion;
 
     private void OnCollisionEnter(Collision other)
     {
         float impactForce = other.impulse.magnitude;
 
-        if(impactForce > forceRequiredToExplode)
+        if(impactForce > GetForceRequiredToExplode())
         {
             Explode();
         }
+    }
+
+    float GetForceRequiredToExplode()
+    {
+        return maxForceRequiredToExplode * (1 - potency);
     }
 
     void Explode()
@@ -31,4 +39,17 @@ public class Explosive : MonoBehaviour, IScannable
     {
         return "Explosive!";
     }
+
+    public void AddToOther(Transform other)
+    {
+        Explosive otherExplosive = other.GetComponent<Explosive>();
+        otherExplosive = otherExplosive == null ? other.gameObject.AddComponent<Explosive>() : otherExplosive;
+
+        otherExplosive.AddEffect(potency);
+    }
+    private void AddEffect(float potency)
+    {
+        this.potency += potency * 0.01f;
+    }
+
 }
