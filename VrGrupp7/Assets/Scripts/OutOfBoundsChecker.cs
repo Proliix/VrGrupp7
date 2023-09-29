4356d6e3e62e112539f83b4d031fed72cfc94a01
Proliix <lucas.andreasson@hotmail.com>
2023-09-29 16:07:38 +0200

    changed debug.log

--- VrGrupp7/Assets/Scripts/OutOfBoundsChecker.cs
+++ VrGrupp7/Assets/Scripts/OutOfBoundsChecker.cs
@@ -7,7 +7,7 @@ public class OutOfBoundsChecker : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("collided with " + other.gameObject.name, other);
+        Debug.Log("OOB checker collided with " + other.gameObject.name, this);
 
         if (other.GetComponent<Respawnable>() != null)
         {
